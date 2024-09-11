3f3ae9c6433680f999217c603633bf2b720eb8e8
locphuocdang <dangloc2407@gmail.com>
2024-09-11 10:39:02 +0300

    iteration 2 done by Loc

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,9 +1,20 @@
-const Service = ({ icon, title, text }) => {
+import React, { useState } from "react";
+
+const Service = ({ id, icon, title, text, onDelete }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleDelete = () => {
+    onDelete(id); // Pass the item's ID to the parent component for deletion
+  };
+
   return (
     <article className="service">
       <span className="service-icon">
         <i className={icon}></i>
       </span>
+      <button onClick={handleDelete} className="delete-button">
+        Delete
+      </button>
       <div className="service-info">
         <h4 className="service-title">{title}</h4>
         <p className="service-text">{text}</p>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,15 +1,24 @@
-import { services } from '../data'
 import Title from './Title'
 import Service from './Service'
+import { useState } from 'react';
+import { services } from "../data";
 
-const Services = () => {
+
+function Services() {
+  const [servicesData, setServicesData] = useState(services)
+
+  const handleDeleteServices = (serviceId) => {
+    const updatedServices = servicesData.filter(s => s.id !== serviceId)
+    setServicesData(updatedServices)
+  }
+
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
+        {servicesData.map((service) => {
+          return <Service {...service} key={service.id} onDelete={handleDeleteServices}/>
         })}
       </div>
     </section>
