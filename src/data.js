8b8f11251dc2840fda65cf91d2b0b3d122f46502
locphuocdang <dangloc2407@gmail.com>
2024-09-11 11:07:08 +0300

    update iteration 5

--- src/data.js
+++ src/data.js
@@ -7,7 +7,14 @@ export const pageLinks = [
   { id: 1, href: "/home", text: "home" },
   { id: 2, href: "/about", text: "about" },
   { id: 3, href: "/services", text: "services" },
-  { id: 4, href: "/tours", text: "tours" },
+  {
+    id: 4,
+    href: "/tours",
+    text: "tours",
+    id: 5,
+    href: "/registration",
+    text: "Registration",
+  },
 ];
 
 export const socialLinks = [
